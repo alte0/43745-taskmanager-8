ea39190fd9c113fe9a8a6d3afac2debb8aca6dbd
m.dmitriev <makc-dmv@yandex.ru>
2019-03-10 23:03:12 +0500

    революция или эволюция?

--- src/modules/data.js
+++ src/modules/data.js
@@ -1,4 +1,4 @@
-import {getRndInteger, getRandomBoolean} from './util';
+import {getRndInteger, getRandomBoolean} from "./util";
 
 const MIN_NUM = 0;
 const maxNums = [2, 4, 1];
@@ -14,12 +14,12 @@ const filters = [
   {
     caption: `OVERDUE`,
     amount: `0`,
-    isDisabled: true,
+    isDisabled: true
   },
   {
     caption: `TODAY`,
     amount: `0`,
-    isDisabled: true,
+    isDisabled: true
   },
   {
     caption: `FAVORITES`,
@@ -36,37 +36,34 @@ const filters = [
   {
     caption: `ARCHIVE`,
     amount: `115`
-  },
+  }
 ];
 /**
  * @return {Object}
  */
 const task = () => ({
-  text: [
-    `Изучить теорию`,
-    `Сделать домашку`,
-    `Пройти интенсив на соточку`,
-  ][getRndInteger(MIN_NUM, maxNums[0])],
+  title: [`Изучить теорию`, `Сделать домашку`, `Пройти интенсив на соточку`][
+    getRndInteger(MIN_NUM, maxNums[0])
+  ],
   dueDate: Date.now() + 1 + Math.floor(Math.random() * 7) * 24 * 60 * 60 * 1000,
-  tags: [
-    new Set([`homework`, `theory`, `practice`]),
-    ``
-  ][getRndInteger(MIN_NUM, maxNums[2])],
+  tags: [new Set([`homework`, `theory`, `practice`]), ``][
+    getRndInteger(MIN_NUM, maxNums[2])
+  ],
   picture: `//picsum.photos/100/100?r=${Math.random()}`,
-  color: [
-    `black`, `yellow`, `blue`, `green`, `pink`
-  ][getRndInteger(MIN_NUM, maxNums[1])],
+  color: [`black`, `yellow`, `blue`, `green`, `pink`][
+    getRndInteger(MIN_NUM, maxNums[1])
+  ],
   repeatingDays: {
-    'mo': getRandomBoolean(),
-    'tu': getRandomBoolean(),
-    'we': getRandomBoolean(),
-    'th': getRandomBoolean(),
-    'fr': getRandomBoolean(),
-    'sa': getRandomBoolean(),
-    'su': getRandomBoolean(),
+    mo: getRandomBoolean(),
+    tu: getRandomBoolean(),
+    we: getRandomBoolean(),
+    th: getRandomBoolean(),
+    fr: getRandomBoolean(),
+    sa: getRandomBoolean(),
+    su: getRandomBoolean()
   },
   isFavorite: getRandomBoolean(),
-  isDone: getRandomBoolean(),
+  isDone: getRandomBoolean()
 });
 
 while (i < 8) {
